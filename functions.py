16e5c1fed3e66f967619ff0c860df04290efe004
Kostas <zaikin399@mail.ru>
2024-01-10 21:38:00 +0300

    first Push code

--- functions.py
+++ functions.py
@@ -35,16 +35,13 @@ for i in last_list:
     dt = datetime.datetime.fromisoformat(data_list)
     if i.get('from'):
         card_number = i['from']
-
         name_card = card_number.split()[0]
         card_show = card_number.split()[-1]
         private_number = card_show[:6] + (len(card_show[6:-4]) * '*') + card_show[-4:]
         chunks, chunk_size = len(private_number), len(private_number) // 4
-
         card_show_to = i['to']
         name_card_to = card_show_to.split()[0]
         card_to = i['to'][-4:].rjust(len(i['to'][-6:]), "*")
-
         print(dt.date().strftime('%d.%m.%Y'), i['description'])
         print(name_card," ".join([private_number[i:i + chunk_size] for i in range(0, chunks, chunk_size)]),"->", name_card_to, card_to)
         print(i['operationAmount']['amount'], i['operationAmount']['currency']['name'])
